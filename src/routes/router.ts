10896291bd8a01011d7578166a525fba440a2cc8
WictorHiago <wictor.hsc@gmail.com>
2022-11-27 05:25:40 -0400

    feat create route register user

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,6 +2,17 @@ import {Router,Request,Response} from 'express'
 
 const router = Router()
 
+router.post('/api/user',(req:Request,res:Response)=>{
+    const name:string = req.body.name
+    const email:string = req.body.email
+    const password:string = req.body.password
+
+    !name ? res.status(401).json({401:"unauthorized."}):
+    !email ? res.status(401).json({401:"unauthorized"}):
+    !password ? res.status(401).json({401:"unauthorized"}):
+    res.status(201).json({201:'user created'})
+})
+
 router.get('/',(req:Request,res:Response)=>{
     res.status(200).json({message:'welcome to server Express'})
 })
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import routes from './routes/router'
 const server = express();
 const port = 5434;
 
+server.use(express.json())
 server.use(routes)
 
 server.listen(port, () => console.log('server listening on port:' + port));
